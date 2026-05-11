ccfcf2a6078f75b4f27d37fe3fc8ec0444926b90
Max <95097703@est.ort.edu.ar>
2026-05-11 11:44:08 -0300

    Probar con productos, creo que funciono solo por el hecho de que en BD no hay datos

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,10 @@
-const mysql = require('mysql2');
+import mysql from 'mysql2';
+
 const pool = mysql.createPool({
     host: 'A-PHZ2-CIDI-04',
     user: 'root',
     password: '',
     database: 'Catanova'
 });
-module.exports = pool.promise();
+
+export default pool.promise();
--- src/modules/productos/productos.model.js
+++ src/modules/productos/productos.model.js
@@ -1,4 +1,4 @@
-import db from "../../database/DB";
+import db from "../../config/database.js";
 
 export const getAllProductos = () => {
   return db.query("SELECT * FROM productos");
