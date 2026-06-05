1c7319ee1023793ba0be809e40b0c1a446c37816
Android Studio <devandroid@ort>
2026-06-05 11:16:03 -0300

    Update

--- src/config/db-config.js
+++ src/config/db-config.js
@@ -1,8 +1,8 @@
 import pg from 'pg';
 import 'dotenv/config';
+
 const { Pool } = pg;
 
-// 1. Definimos el objeto de configuración
 const DBConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
@@ -11,11 +11,8 @@ const DBConfig = {
     database: process.env.DB_NAME
 };
 
-// 2. Creamos la instancia del Pool (esto es lo que realmente hace las consultas)
 const db = new Pool(DBConfig);
 
-// 3. Log para confirmar que cargó
 console.log('--- Intentando conectar a la base de datos: ', DBConfig.database, '---');
 
-// IMPORTANTE: Exportamos 'db', que es el Pool, no solo el texto de configuración
 export default db;
