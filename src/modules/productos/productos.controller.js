473491d70794e1824ea518dbbd341fb909771a33
Android Studio <devandroid@ort>
2026-06-08 09:18:17 -0300

    Creacion de modulos reutilizables para mejorar la legibilidad del codigo

--- src/modules/productos/productos.controller.js
+++ src/modules/productos/productos.controller.js
@@ -1,6 +1,21 @@
 import { getProductosActivos, agregarProducto, buscarProductoPorNombre, getAllProductos, modificarEstado, getProductosId, modificarProducto } from "./productos.model.js";
-import { getTiendaId } from "../tiendas/tiendas.controller.js" 
-export const GetProductosActivos = async(req, res) => {
+import { getTiendaId } from "../tiendas/tiendas.controller.js"
+
+// ==========================================
+// Modulos de ayuda que se pueden reutilizar
+// ==========================================
+export const verificarExistenciaTienda = async (id_tienda) => {
+    const existeTienda = await getTiendaId(id_tienda);
+    if (!existeTienda) throw new Error("El id tienda no existe");
+}
+
+const verificarExistenciaProducto = async (id_producto) => {
+    const productoActual = await getProductosId(id_producto);
+    if (!productoActual) throw new Error("El producto no existe");
+    return productoActual;
+};
+
+export const GetProductosActivos = async (req, res) => {
     try {
         const filasModificadas = await getProductosActivos();
         if (!filasModificadas || filasModificadas.length === 0) {
@@ -14,17 +29,13 @@ export const GetProductosActivos = async(req, res) => {
     }
 };
 
-export const insertProducto = async({ nombre, precio, stock, imagen, activo, id_tienda }) => {
+export const insertProducto = async ({ nombre, precio, stock, imagen, activo, id_tienda }) => {
 
     const existe = await buscarProductoPorNombre(nombre);
     if (existe) {
         throw new Error("El producto ya existe");
     }
-    const existeTienda = await getTiendaId(id_tienda)
-    if (!existeTienda) {
-        throw new Error("El id tienda no existe");
-
-    }
+    await verificarExistenciaTienda(id_tienda);
     const producto = {
         nombre,
         precio: Number(precio),
@@ -37,7 +48,7 @@ export const insertProducto = async({ nombre, precio, stock, imagen, activo, id_
     return await agregarProducto(producto);
 };
 
-export const getProductos = async() => {
+export const getProductos = async () => {
     const listaProductos = await getAllProductos();
     if (listaProductos.length === 0) {
         throw new Error("No hay productos cargados");
@@ -46,16 +57,13 @@ export const getProductos = async() => {
     return listaProductos;
 }
 
-export const updateEstadoProducto = async(id, estado) => {
-    const existeProducto = await getProductosId(id);
-    if (!existeProducto) {
-        throw new Error("El producto ingresado no existe");
-    }
+export const updateEstadoProducto = async (id, estado) => {
+    await verificarExistenciaProducto(id);
     const productoActualizado = await modificarEstado(id, estado);
     return productoActualizado;
 }
 
-export const actualizarProducto = async(datosProducto) => {
+export const actualizarProducto = async (datosProducto) => {
     const { id_producto, nombre, precio, stock, imagen, activo, id_tienda } = datosProducto;
     if (!id_producto) throw new Error("El ID del producto es obligatorio.");
     if (!nombre || precio === undefined || stock === undefined || id_tienda === undefined) {
@@ -73,12 +81,7 @@ export const actualizarProducto = async(datosProducto) => {
             throw new Error("El nuevo nombre ya está en uso");
         }
     }
-    const existeTienda = await getTiendaId(id_tienda) //hacer despues una funcion que se encarge de hacer esto para evitar codigo repetido
-
-    if (!existeTienda) {
-        throw new Error("El id tienda no existe");
-
-    }
+    await verificarExistenciaTienda(id_tienda);
 
     const productoFormateado = {
         id_producto: Number(id_producto),
