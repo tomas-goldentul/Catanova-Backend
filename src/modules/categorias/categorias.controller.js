d297d3dba0e4c009ec4e6d9ebc247894b977bb0d
Tomas Goldentul <goldentultomas@gmail.com>
2026-06-10 08:54:52 -0300

    Validacion en categorias

--- src/modules/categorias/categorias.controller.js
+++ src/modules/categorias/categorias.controller.js
@@ -2,7 +2,9 @@ import * as categoriasModel from "./categorias.model.js";
 
 export const insertCategoria = async (nombre)=>{
     const existeNombreCategoria = await categoriasModel.getCategoriaPorNombre(nombre);
-   
+    if (nombre.trim() === ""){
+        throw new Error (`Ingresa un nombre`)
+    }
     if (existeNombreCategoria){
         throw new Error (`Ya existe el nombre de categoria: ${nombre}`)
     }
