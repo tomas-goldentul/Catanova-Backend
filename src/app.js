9d4a0c09a42ff39021b8de9b5f1d7d02b74f915b
Android Studio <devandroid@ort>
2026-06-22 12:09:59 -0300

    Commiteando cosas+

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express"; 
 import cors from "cors";
 
-import dotenv from "dotenv"; //npm i dotnev
+import dotenv from "dotenv"; //npm i dotnev 
 dotenv.config();
 
 import tiendasRoutes from "./modules/tiendas/tiendas.routes.js";
